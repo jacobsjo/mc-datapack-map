321d81f0d5234c2e1a56186ad0770ee17e15727d
Jochen Jacobs <JochenJacobs33@gmail.com>
2023-09-09 23:13:12 +0200

    add legal note

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -9,6 +9,7 @@ import LocaleChanger from './LocaleChanger.vue';
         <LocaleChanger />
         <div class="smallprint">
             <p>Copyright © 2023 jacobsjo</p>
+            <p id="note">{{  $t('footer.legel.note') }}</p>
             <a href="https://jacobsjo.eu/about.html" target="_blank">{{ $t('footer.about') }}</a>
             <a href="https://github.com/jacobsjo/mc-datapack-map" target="_blank">{{ $t('footer.view_source') }}</a>
             <a href="https://github.com/jacobsjo/mc-datapack-map/issues" target="_blank">{{ $t('footer.report_issue') }}</a>
@@ -28,20 +29,24 @@ import LocaleChanger from './LocaleChanger.vue';
         display: flex;
         flex-direction: row;
         flex-wrap: wrap;
-        height: 2.9rem;
         gap: 0.5rem;
         row-gap: 0rem;
+        height: 6.5rem;
     }
 
     a, p{
         color: rgb(179, 179, 179);
-        height: 0rem;
     }
 
     p {
         display: inline;
         padding: 0;
         margin: 0;
+    }
+
+    #note {
+        font-size: 8pt;
+        height: 2.8rem;
     }
 
     a {
