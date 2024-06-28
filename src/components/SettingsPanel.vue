9bc59519569f53933412fc5ada445e11d7fd5d14
Jochen Jacobs <JochenJacobs33@gmail.com>
2024-06-28 13:53:28 +0200

    Improve checking if number, typescript typing

--- src/components/SettingsPanel.vue
+++ src/components/SettingsPanel.vue
@@ -19,23 +19,22 @@ datapackStore.$subscribe(async (mutation, state) => {
 function randomizeSeed() {
     settingsStore.seed = random.nextLong()
 }
-function alphanumeric_map_seed(input_string) {
-    //check if string is already a number
-    //https://stackoverflow.com/a/1421988
-    if (!isNaN(parseFloat(input_string)) && !isNaN(input_string - 0)) {
-        return input_string
-    }
 
-    //String hashCode() function from Java
-    //https://stackoverflow.com/questions/7616461/generate-a-hash-from-string-in-javascript
-    var hash = 0, i, chr;
-    if (input_string.length === 0) return hash;
-    for (i = 0; i < input_string.length; i++) {
-        chr = input_string.charCodeAt(i);
-        hash = ((hash << 5) - hash) + chr;
-        hash |= 0; // Convert to 32bit integer
+function parseSeed(input: string): bigint {
+    try {
+        return BigInt(input)
+    } catch {
+        //String hashCode() function from Java
+        //https://stackoverflow.com/questions/7616461/generate-a-hash-from-string-in-javascript
+        var hash = 0, i, chr;
+        if (input.length === 0) return BigInt(0);
+        for (i = 0; i < input.length; i++) {
+            chr = input.charCodeAt(i);
+            hash = ((hash << 5) - hash) + chr;
+            hash |= 0; // Convert to 32bit integer
+        }
+        return BigInt(hash);
     }
-    return hash;
 }
 </script>
 
@@ -70,11 +69,7 @@ function alphanumeric_map_seed(input_string) {
             <font-awesome-icon icon="fa-dice" class="button" tabindex="0" @click="randomizeSeed"
                 @keypress.enter="randomizeSeed" :title="$t('settings.seed.randomize_button.title')" />
             <input :aria-label="$t('settings.seed.aria-label')" :value="settingsStore.seed" @change="event => {
-                try {
-                    settingsStore.seed = BigInt(alphanumeric_map_seed((event.target as HTMLInputElement).value))
-                } catch {
-                    (event.target as HTMLInputElement).value = settingsStore.seed.toString()
-                }
+                settingsStore.seed = parseSeed((event.target as HTMLInputElement).value)
             }" type="text" />
 
         </div>
