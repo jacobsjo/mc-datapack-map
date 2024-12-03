53dc969c9fdf25a0fc0570933ca7e5f1b2ad1962
Jochen Jacobs <JochenJacobs33@gmail.com>
2024-12-03 01:27:43 +0100

    update to 1.21.4
    + ensure no dimension padding on old versions

--- src/stores/useSettingsStore.ts
+++ src/stores/useSettingsStore.ts
@@ -8,7 +8,7 @@ import { parseSeed, updateUrlParam, versionMetadata } from "../util";
 
 export const useSettingsStore = defineStore('settings', () => {
     const defaults = {
-        mc_version: "1_21_3",
+        mc_version: "1_21_4",
         world_preset: "minecraft:normal",
         dimension: "minecraft:overworld",
         seed: "0"
