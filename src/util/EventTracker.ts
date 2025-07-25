d810df61ec757f8a0edd812be9af96ddab78732c
Jochen Jacobs <JochenJacobs33@gmail.com>
2025-07-26 01:41:34 +0200

    reduce event tracking

--- src/util/EventTracker.ts
+++ src/util/EventTracker.ts
@@ -1,6 +1,8 @@
 export namespace EventTracker {
+    const doTrack = Math.random() < 0.2
+
     export function track(event: string ){ 
-        if ((window as any).beam !== undefined) {
+        if ((window as any).beam !== undefined && doTrack) {
             (window as any).beam("/event/" + event)
         }
     }
